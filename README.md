68b6f1c18b2fa88d4c2f9740b6ce5e017d6de47c
Andrew Bloom <blooma116@gmail.com>
2018-03-18 00:03:24 -0700

    wrote tests for linked list

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # data-structures
 This is a collection of coded examples of various data structures
+
+
+## Credits
+
+Created by Andrew Bloom.
--- cpp/linked-list/linked-list.cpp
+++ cpp/linked-list/linked-list.cpp
@@ -3,118 +3,119 @@
 #include <iostream>
 #include "linked-list.hpp"
 
-class LinkedList {
-    public:
-        LinkedList(void) {
-            Node *head = NULL;
-            Node *tail = NULL;
-            int length = 0;
-        }
-        ~LinkedList(void){
-            Node *temp = head;
-            while(temp != NULL) {
-                Node *next = temp->next;
-                delete temp;
-                temp = next;
-                length--;
-            }
-            head = NULL;
-        }
-        void prependNode(int value) {
-            Node *temp = new Node;
-            temp->data = value;
-            if (head != NULL) {
-                temp->next = head;
-            } else {
-                tail = temp;
-            }
-            head = temp;
-            length++;
-        }
-        void appendNode(int value) {
-            Node *temp = new Node;
-            temp->data = value;
-            if (tail != NULL) {
-                tail->next = temp;
-            } else {
-                head = temp;
-            }
-            tail = temp;
-            length++;
-        }
-        int removeNode(int value) {
-            if (head == NULL) {
-                return -1;
-            }
-            if (head->data == value) {
-                if (head == tail) {
-                    delete tail;
-                    tail = NULL;
-                    head = NULL;
-                    length--;
-                    return 0;
-                } else {
-                    Node *temp = head->next;
-                    delete head;
-                    head = temp;
-                    length--;
-                    return 0;
-                }
-            } else {
-                Node *temp = head;
-                while(temp != NULL) {
-                    // still need to account for condition
-                    // where tail is the value to be removed
-                    Node *next = temp->next;
-                    if (next->data == value) {
-                        temp->next = next->next;
-                        delete next;
-                        length--;
-                        return 0;
-                    }
-                    temp = next;
-                }
-            }
-            return -1;
-        }
-        int peek(void){
-            if (head == NULL) {
-                return -1;
-            } else {
-                return head->data;
-            }
-        }
-        bool isEmpty(void){
-            if (head == NULL) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        bool has(int value){
-            Node *temp = head;
-                while (temp != NULL) {
-                    if (temp->data == value) {
-                        return true;
-                    }
-                    temp = temp->next;
-                }
-            return false;
-        }
-        int getLength(void) {
-            return length;
-        }
-        void print(void){
-            Node *temp = head;
-                while (temp != NULL) {
-                    std::cout << temp->data << std::endl;
-                    temp = temp->next;
-                }
-        }
-    private:
-        Node *head, *tail;
-        int length = 0;
-};
+using namespace std;
 
-        
-        
+LinkedList::LinkedList(void) {
+    head = NULL;
+    tail = NULL;
+    length = 0;
+}
+LinkedList::~LinkedList(void){
+    Node *temp = head;
+    while(temp != NULL) {
+        Node *next = temp->next;
+        delete temp;
+        temp = next;
+        length--;
+    }
+    head = NULL;
+    tail = NULL;
+}
+void LinkedList::prependNode(int value) {
+    Node *temp = new Node;
+    temp->data = value;
+    temp->next = NULL;
+    if (head != NULL) {
+        temp->next = head;
+    } else {
+        tail = temp;
+    }
+    head = temp;
+    temp = NULL;
+    length++;
+}
+void LinkedList::appendNode(int value) {
+    Node *temp = new Node;
+    temp->data = value;
+    temp->next = NULL;
+    if (tail != NULL) {
+        tail->next = temp;
+    } else {
+        head = temp;
+    }
+    tail = temp;
+    temp = NULL;
+    length++;
+}
+int LinkedList::removeNode(int value) {
+    if (head == NULL) {
+        return -1;
+    }
+    if (head->data == value) {
+        if (head == tail) {
+            delete tail;
+            tail = NULL;
+            head = NULL;
+            length--;
+            return 0;
+        } else {
+            Node *temp = head->next;
+            delete head;
+            head = temp;
+            temp = NULL;
+            length--;
+            return 0;
+        }
+    } else {
+        Node *temp = head;
+        while(temp != NULL) {
+            Node *next = temp->next;
+            if (next->data == value) {
+                if (temp->next == tail) {
+                    tail = temp;
+                }
+                temp->next = next->next;
+                delete next;
+                next = NULL;
+                length--;
+                return 0;
+            }
+            temp = next;
+        }
+    }
+    return -1;
+}
+int LinkedList::peek(void){
+    if (head == NULL) {
+        return -1;
+    } else {
+        return head->data;
+    }
+}
+bool LinkedList::isEmpty(void){
+    if (head == NULL) {
+        return true;
+    } else {
+        return false;
+    }
+}
+bool LinkedList::has(int value){
+    Node *temp = head;
+        while (temp != NULL) {
+            if (temp->data == value) {
+                return true;
+            }
+            temp = temp->next;
+        }
+    return false;
+}
+int LinkedList::getLength(void) {
+    return length;
+}
+void LinkedList::print(void){
+    Node *temp = head;
+        while (temp != NULL) {
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+}
