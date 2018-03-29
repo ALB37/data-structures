3fc7dee60c04bee876536d979b34ec24289adfd5
Andrew Bloom <blooma116@gmail.com>
2018-03-28 17:22:29 -0700

    min-heap functioning correctly, may implement more error checking

--- python/min-heap/lib/min_heap.py
+++ python/min-heap/lib/min_heap.py
@@ -4,27 +4,30 @@ class MinHeap:
 
     def peek(self):
         if not self._data:
-            return None
+            return
         else:
             return self._data[0]
     
     def insert(self, value):
+        if type(value) not in [int, float]:
+            raise TypeError("Min Heap can only take numbers as values")
+
         self._data.append(value)
         self._bubbleUp(len(self._data) - 1)
 
     def _bubbleUp(self, index):
-        if not index:
+        if index == 0:
             return
         
-        parentIndex = self._parentIndex(index)
+        parent = self._parentIndex(index)
 
-        if self._data[parentIndex] > self._data[index]:
-            self._swap(parentIndex, index)
-            self._bubbleUp(parentIndex)
+        if self._data[parent] > self._data[index]:
+            self._swap(parent, index)
+            self._bubbleUp(parent)
     
     @staticmethod
     def _parentIndex(index):
-        return index - 1 // 2
+        return (index - 1) // 2
 
     def _swap(self, i, j):
         temp = self._data[i]
@@ -34,7 +37,7 @@ class MinHeap:
 
     def extractMin(self):
         if not self._data:
-            return None
+            return
 
         min_val = self._data[0]
         last_val = self._data.pop()
@@ -47,11 +50,11 @@ class MinHeap:
 
     @staticmethod
     def _leftChildIndex(index):
-        return 2 * index + 1
+        return (2 * index) + 1
 
     @staticmethod
     def _rightChildIndex(index):
-        return 2 * index + 2
+        return (2 * index) + 2
 
     def _bubbleDown(self, index):
         min_index = index
