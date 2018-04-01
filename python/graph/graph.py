e8dce23cc130683b61af89632af56da853f8e8b3
Andrew Bloom <blooma116@gmail.com>
2018-03-31 21:57:37 -0700

    finished implementing basic graph methods

--- python/graph/graph.py
+++ python/graph/graph.py
@@ -2,19 +2,25 @@ class Graph:
     def __init__(self):
         self._adjacencies = {}
 
-    def addNode(self, nodeCoordinates):
-        # node will be a tuple of the form (x, y)
-        self._adjacencies[nodeCoordinates] = []
+    def addNode(self, coordinates):
+        # coordinates will be a tuple of the form (x, y)
+        self._adjacencies[coordinates] = []
 
-    def addEdge(self, startNode, endNode, weight=0):
-        if startNode not in self._adjacencies \
-                or endNode not in self._adjacencies:
-            raise ValueError('startNode or endNode does not exist')
+    def addEdge(self, startCoordinates, endCoordinates, weight=0):
+        if startCoordinates not in self._adjacencies \
+                or endCoordinates not in self._adjacencies:
+            raise ValueError('start or end coordinates do not exist')
 
-    
+        adjacentList = self._adjacencies[startCoordinates]
 
+        adjacentList.append({
+            'node' : endCoordinates,
+            'weight' : weight
+        })
 
+    def getNeighbors(self, coordinates):
+        if coordinates not in self._adjacencies:
+            raise ValueError('coordinates not in graph')
 
+        return self._adjacencies[coordinates][:]
 
-
-            
